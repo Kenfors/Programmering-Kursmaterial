8f62fe052e52cf9b127314db362211cd8b0a7149
KaEnfors <37064780+KaEnfors@users.noreply.github.com>
2019-12-19 09:38:46 +0100

    Stuff

---
+++ Python/Programmering2/Arcade/asteroid1/main.py
@@ -0,0 +1,18 @@
+import random
+import math
+import arcade
+import os
+
+
+
+class Asteroid(arcade.Window):
+    pass
+
+
+def main():
+    window = Asteroid()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
--- Python/Programmering2/OO/cards.py
+++ Python/Programmering2/OO/cards.py
@@ -10,12 +10,22 @@ class Card():
         pass
 
 class CardHolder():
+    cards : list
     
     def __init__(self):
         pass
 
     def __str__(self):
         pass
+    def pushCard(self, theCard):
+        
+        self.cards.append(theCard)
+
+        return
+
+    def pickCard(self, position):
+        pickedCard = self.cards.pop(position)
+        return pickedCard
 
 class CardGame():
     
@@ -24,6 +34,13 @@ class CardGame():
 
     def __str__(self):
         pass
+
+    def Start(self):
+        # Töm spelbräde
+        # Skapa kortlek/deck
+        # Sätt startvärden på players[]
+
+        return
 
 
 class BlackJack(CardGame): #Ärver ALLT från "CardGame"
